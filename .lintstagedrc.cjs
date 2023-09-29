d112efaa0dd63373daef67cc8fed7bc249b7ff9a
trelcray <thaliszambarda@gmail.com>
2023-09-29 15:51:00 -0300

    revert: remove lintstagedrc config

--- .lintstagedrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "*.{js,jsx,ts,tsx}": (filenames) => [
-    `next lint --fix . ${filenames.join(' --file ')}`,
-  ],
-  "*.{ts,tsx,css}": (filenames) => [
-    `prettier --write . ${filenames.join(' --file ')}`,
-  ],
-}
